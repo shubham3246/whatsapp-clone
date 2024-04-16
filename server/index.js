f429adba9cb7f7b1b340c28625cba791c4fcc10b
Shubham kumar patel <93185837+shubham3246@users.noreply.github.com>
2024-04-16 17:41:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors:{
-    origin: "http://localhost:3000",
+    origin: "https://whatsapp-clone-rouge.vercel.app/",
     methods: ["POST", "GET"],
     credentials: true
   }
